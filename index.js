7f6dc3088a2094f337873b5edbba5aac4dd0b8d4
Bektur <addimilan611@gmail.com>
2025-07-23 19:01:17 +0600

    getting information about people

---
+++ index.js
@@ -0,0 +1,25 @@
+let result = document.querySelector(".result")
+
+function getProfile() {
+    fetch('https://dummyjson.com/users')
+    .then((res) => {
+        return res.json()
+    }) .then((window) => {
+        console.log(window);
+        result.innerHTML = window.users.map((item) => {
+        return`
+        <div class="card">
+        <img src="${item.image}"/>
+        <p class="candidate">Candidate №${item.id}</p>
+        <h2 class="name"><span class="span">Name:</span> ${item.firstName} ${item.lastName}</h2>
+        <p class="birth"><span class="span">Was born on</span> ${item.birthDate}, 
+        <span class="span">in</span> ${item.address.state}, ${item.address.city}</p>
+        <p class="phoneNumber"><span class="span">Phone number:</span> <span class="number">${item.phone}</span></p>
+        <p class="education"><span class="span">Graduation</span> <span class="university">"${item.university}"</span></p>
+        </div>
+        `
+        }).join("")
+    })
+}
+
+getProfile()
