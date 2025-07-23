dae18944d499058a56a0fdfc75a2f55e19130bf4
Bektur <addimilan611@gmail.com>
2025-07-23 19:10:08 +0600

    fixed some details

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function getProfile() {
         <img src="${item.image}"/>
         <p class="candidate">Candidate №${item.id}</p>
         <h2 class="name"><span class="span">Name:</span> ${item.firstName} ${item.lastName}</h2>
-        <p class="birth"><span class="span">Was born on</span> ${item.birthDate}, 
-        <span class="span">in</span> ${item.address.state}, ${item.address.city}</p>
+        <p class="birth"><span class="span">Was born on</span> ${item.birthDate}, </p>
+        <p class="country"><span class="span">in</span> ${item.address.state}, ${item.address.city}</p>
         <p class="phoneNumber"><span class="span">Phone number:</span> <span class="number">${item.phone}</span></p>
         <p class="education"><span class="span">Graduation</span> <span class="university">"${item.university}"</span></p>
         </div>
